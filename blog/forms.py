90ec6f31219f7999ecbf11e359cef0b65c502b8f
KushalGhimire17 <kushalghimire61@gmail.com>
2020-08-24 19:37:35 +0545

    [Added] Dashboard to CRUDfrom frontend after login

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Notice
+from .models import Notice, Post
 
 class NoticeForm(forms.ModelForm):
     class Meta:
         model = Notice
         fields = ('title','notice','cover')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'image', 'date_posted', 'author')
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post, Notice
 from django.core.files.storage import FileSystemStorage
-from .forms import NoticeForm
+from .forms import NoticeForm, PostForm
 
 def home(request):
     return render(request, 'blog/home.html')
@@ -32,3 +32,26 @@ def read_more(request,id):
         'blog': Post.objects.get(pk=id)
     }
     return render(request, 'blog/read_more.html', context) 
+
+
+def dashboard(request, id=0):
+    if request.method == 'POST':
+        if id == 0:
+            form = PostForm(request.POST, request.FILES)
+        else:
+            posts = Post.objects.get(pk=id)
+            form = PostForm(request.POST, instance=posts)
+        if form.is_valid():
+            form.save()
+        return redirect('post_list')
+    else:
+        if id == 0:
+            form = PostForm()
+        else:
+            posts = Post.objects.get(pk=id)
+            form = PostForm(instance=posts)
+        return render(request, 'blog/dashboard.html', {'form': form, 'posts': Post.objects.all()})
+
+def post_list(request):
+    context = {'post_list': Post.objects.all()}
+    return render(request, 'blog/post_list.html', context)
